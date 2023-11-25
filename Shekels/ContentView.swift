0549005218c6fae2f511bfac3d9c63e6e6b15bce
Joseph DeWeese <jdeweese@weldtascics.systems>
2023-11-24 18:27:04 -0600

    Initial Commit

---
+++ Shekels/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shekels
+//
+//  Created by J. DeWeese on 11/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shekels/ShekelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShekelsApp.swift
+//  Shekels
+//
+//  Created by J. DeWeese on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct ShekelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
